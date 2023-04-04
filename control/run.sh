5b4f206002ad027a690f32d454e5da50bda2b088
Jack Vaughan <jackvaughan09@gmail.com>
2023-04-03 22:07:33 -0500

    return pdfs, filter docs, output logs

--- control/run.sh
+++ control/run.sh
@@ -2,3 +2,6 @@
 venv/bin/python3 clean.py ../data/zip ../data/pdf
 sh convert.sh ../data/pdf
 venv/bin/python3 mksheet.py ../data/pdf
+for f in ../data/pdf/*.doc*; do
+  mv "$f" ../data/doc
+done
--- x64phil.sh
+++ x64phil.sh
@@ -9,8 +9,13 @@ fi
 if [ ! -d "data/xlsx" ]; then
   mkdir -p "data/xlsx"
 fi
+if [ ! -d "data/doc"]; then
+  mkdir -p "data/doc"
+fi
 docker build -t phil:local -f docker/x64.Dockerfile .
 docker run --name philapp phil:local
 docker cp philapp:app/data/xlsx extracted
+docker cp philapp:app/data/pdf extracted/pdfs
+docker logs philapp > logs.txt
 docker container rm -f philapp
 docker image rm -f phil:local
