7b87a1cb1519adde26b4cdd3e3ebda5aca28d3f5
Jack Vaughan <jackvaughan09@gmail.com>
2023-04-18 17:31:39 -0500

    added logging, decreased print verbosity

--- control/clean.py
+++ control/clean.py
@@ -13,6 +13,8 @@ import sys
 from shutil import rmtree
 from zipfile import ZipFile
 import config as c
+from logging_config import setup_logging
+import logging
 
 
 def unzip(diz, diu):
@@ -34,28 +36,18 @@ def remove_junk(di):
         )
         is_pdf = bool(os.path.splitext(f)[1] == ".pdf")
         is_doc = bool(os.path.splitext(f)[1] in [".docx", ".doc"])
-        print(
-            f"""
-        File: 
-        {f.split('/')[-1]}
-        =======================================
-        target filename: {has_filename_target}
-        pdf:             {is_pdf}
-        doc/docx:        {is_doc}
-        keep:            {is_doc or (is_pdf and has_filename_target)}
-        """
-        )
         if is_doc and has_filename_target:
-            print(f"{f} is doc and has target name")
+            logging.info(f"{f} is doc and has target name. Keeping")
             continue
         elif is_pdf and has_filename_target:
-            print(f"{f} is pdf and has target name")
+            logging.info(f"{f} is pdf and has target name. Keeping")
             continue
         elif os.path.isfile(f):
-            print(f"Removing {f}")
+            logging.info(f"Removing {f}")
             os.remove(f)
         else:
             rmtree(f)
+            logging.info(f"Removing {f}")
     di = os.path.join(os.path.dirname(di), "zip")
     z_folders_list = [os.path.join(di, f) for f in os.listdir(di)]
     for f in z_folders_list:
@@ -66,6 +58,7 @@ def remove_junk(di):
 
 
 if __name__ == "__main__":
+    setup_logging()
     zipped_dir = sys.argv[1]
     unzipped_dir = sys.argv[2]
     unzip(zipped_dir, unzipped_dir)
