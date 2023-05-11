1ecd38e3aa2d73cdbb43e7b47b84e12068b90a75
Jack Vaughan <jackvaughan09@gmail.com>
2023-05-11 18:15:28 -0500

    deprecated control module

--- control/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-venv/bin/python3 clean.py ../data/zip ../data/pdf
-sh convert.sh ../data/pdf
-venv/bin/python3 mksheet.py ../data/pdf
-for f in ../data/pdf/*.doc*; do
-  mv "$f" ../data/doc
-done
