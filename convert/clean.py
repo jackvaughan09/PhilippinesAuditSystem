c101807c5f6c026f94aa4e646d4d4ee2419d5994
Jack Vaughan <jackvaughan09@gmail.com>
2023-05-11 18:19:34 -0500

    Refactored and increased coverage

--- convert/clean.py
+++ convert/clean.py
@@ -11,7 +11,17 @@ import sys
 from shutil import rmtree
 from zipfile import ZipFile
 
+from tqdm import tqdm
+
 FILENAME_TARGET = ["Status", "Audit"]
+BAD_NAMES = [
+    "Part-1",
+    "Certificate",
+    "Part1",
+    "Independent",
+    "Part 1",
+]
+VALID_EXTENSIONS = [".pdf", ".docx", ".doc"]
 
 
 def unzip(zipped, unzipped):
@@ -25,27 +35,63 @@ def unzip(zipped, unzipped):
             src.extractall(unzipped)
 
 
-def remove_junk(unzipped_dir):
-    uz_list = [os.path.join(unzipped_dir, f) for f in os.listdir(unzipped_dir)]
-    for f in uz_list:
-        has_filename_target = any(
-            [target in os.path.splitext(f)[0] for target in FILENAME_TARGET]
-        )
-        is_pdf = bool(os.path.splitext(f)[1] == ".pdf")
-        is_doc = bool(os.path.splitext(f)[1] in [".docx", ".doc"])
-        if is_doc and has_filename_target:
+def is_duplicate(filename, seen):
+    if filename in seen:
+        return True
+    seen.add(filename)
+    return False
+
+
+def has_bad_name(filename):
+    return any(bad_name in filename for bad_name in BAD_NAMES)
+
+
+def has_target_and_valid_extension(filename):
+    has_filename_target = any(
+        target in os.path.splitext(filename)[0] for target in FILENAME_TARGET
+    )
+    file_extension = os.path.splitext(filename)[1]
+    return has_filename_target and file_extension in VALID_EXTENSIONS
+
+
+def clean_directory(directory: str) -> None:
+    """Remove duplicate files, specific unwanted files, and junk files from a directory.
+    Args:
+        - directory (str): The directory to clean.
+    """
+    print(
+        f"Removing unwanted files...\n\
+    Current file count: {len(os.listdir(directory))}"
+    )
+    seen = set()
+    for filename in tqdm(os.listdir(directory)):
+        file_path = os.path.join(directory, filename)
+
+        if is_duplicate(filename, seen):
+            print("Duplicate file: " + filename)
+            os.remove(file_path)
             continue
-        elif is_pdf and has_filename_target:
+
+        if has_bad_name(filename):
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+            else:
+                rmtree(file_path)
             continue
-        elif os.path.isfile(f):
-            os.remove(f)
-        else:
-            rmtree(f)
+
+        if not has_target_and_valid_extension(filename):
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+            else:
+                rmtree(file_path)
+            continue
+
+    print(f"Now there are: {len(os.listdir(directory))} files")
 
 
 def main(zipped_dir, unzipped_dir):
     unzip(zipped_dir, unzipped_dir)
-    remove_junk(unzipped_dir)
+    clean_directory(unzipped_dir)
 
 
 if __name__ == "__main__":
