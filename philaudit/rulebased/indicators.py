954916d038c9eb7232da687896c974326db83415
Jack Vaughan <jackvaughan09@gmail.com>
2023-06-21 15:37:06 -0500

    deprecated but retaining old code for now

---
+++ philaudit/rulebased/indicators.py
@@ -0,0 +1,29 @@
+TARGETS = [
+    "status of implementation of prior years’ audit recommendations",
+    "status of implementation of prior year's audit recommendations",
+    "status of implementation of prior year’s recommendations",
+    "status of implementation of prior years’ audit recommendation",
+    "status of implementation of prior years’ recommendations",
+    "status of implementation of prior year audit recommendations",
+    "status of prior year’s recommendations",
+    "status of implementation of prior years’ audit recommendati ons",
+    "status of implementation of prior year’s audit recommendations",
+    "status of implementation for prior year’s recommendations",
+    "status of implementation of prior year’s audit recommendat ions",
+    "status of implementation of prior years ’ audit recommendations",
+    "status of implementation of prior years’saudit recommendations",
+]
+ANNEXES = ["annex", "annexes", "a n n e x", "a n n e x e s"]
+# "annex" and "annexes" are too general to be used as p4 indicators reliably.
+PART_4_INDICATORS = [
+    "part iv",
+    "iv. annex",
+    "a n n e x e s",
+    "annexes",
+    "pa rt iv",
+    "p art iv",
+    "part i v",
+    "part i v.",
+    "part iv.",
+    "part iv. annex",
+]
